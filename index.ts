262804ecf46d1a097eb4758dbe92aa19c8bfaaaf
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-03-12 21:36:24 -0700

    Added: routers

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-//Hello World
+import express, {Express, Request, Response} from 'express';
+import apiRouter from './src/routes'
+const morgan = require('morgan');
+
+const app: Express = express();
+const port = 3007;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(apiRouter);
+
+app.listen(port, ()=> {
+    console.log(`App listening on ${port}`);
+});
