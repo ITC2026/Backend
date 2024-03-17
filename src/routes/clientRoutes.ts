ddcae005dbbaf6be9db227cd67d1e8b1601aae6a
martín tánori <119524408+m6rtes@users.noreply.github.com>
2024-03-17 22:18:10 +0000

    clientRoutes added

---
+++ src/routes/clientRoutes.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import { createClient, deleteClient, getAllClients, getClientById, modifyClient } from '../controllers/clientControllers';
+
+const clientRouter: Router = Router();
+
+clientRouter.get('/', getAllClients);
+
+clientRouter.get('/:id', getClientById);
+
+clientRouter.post('/', createClient);
+
+clientRouter.delete('/', deleteClient);
+
+clientRouter.patch('/:id', modifyClient);
+
+export default clientRouter;
