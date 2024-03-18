4a7b76668675064e65fc67b0ec2261a334b57de7
Santiago Eduardo Poblete Talamante <133782845+santiagopoblete@users.noreply.github.com>
2024-03-18 06:30:33 +0000

    Client controller now has validations

--- src/controller/clientController.ts
+++ src/controller/clientController.ts
@@ -1,5 +1,52 @@
 import { RequestHandler, Request, Response } from 'express';
 import { Client } from "../models/client";
+import validator from 'validator';
+
+// Retrieve all Clients from the database.
+export const getAllClients: RequestHandler = (req: Request, res: Response) => {
+  Client.findAll()
+  .then((data: Client[]) => {
+    return res.status(200).json({
+      status: "success",
+      message: "Clients succesfully retrieved",
+      payload: data,
+    });
+  })
+  .catch((err) => {
+    return res.status(500).json({
+      status: "error",
+      message: "Something happened retrieving all clients. " + err.message,
+      payload: null,
+    });
+  });
+};
+
+// Find a single Client with an id
+export const getClientById: RequestHandler = (req: Request, res: Response) => {
+  Client.findByPk(req.params.id)
+    .then((data: Client | null) => {
+      if (data) {
+        return res.status(200).json({
+            status: 'success',
+            message: 'Client retrieved successfully',
+            payload: data
+        });
+      } else {
+        return res.status(404).json({
+            status: 'error',
+            message: 'Client not found',
+            payload: null
+        });
+      }
+  })
+  .catch((err) => {
+      return res.status(500).json({
+          status: "error",
+          message: "There was an error finding the Client." + err.message,
+          payload: null,
+      });
+  });
+};
 
 // Create and Save a New Client
 export const createClient: RequestHandler = (req: Request, res: Response) => {
@@ -11,6 +58,26 @@ export const createClient: RequestHandler = (req: Request, res: Response) => {
       payload: null,
     });
   }
+
+  const { contract_pdf_url, logo_url, client_name, client_desc, exclusivity, high_growth, division } = req.body;
+
+    //Validations
+    if (!contract_pdf_url || !logo_url || !client_name || !client_desc || !exclusivity || !high_growth || !division) {
+      return res.status(400).json({
+          status: 'error',
+          message: 'All fields are required',
+          payload: null
+      });
+    }
+
+    if (!validator.isURL(contract_pdf_url)) {
+      return res.status(400).json({ message: 'Invalid URL format for contract' });
+    }
+
+    if (!validator.isURL(logo_url)) {
+      return res.status(400).json({ message: 'Invalid URL format for logo' });
+    }
+
 
   // Save Client in the Database
   const client = { ...req.body };
@@ -31,47 +98,6 @@ export const createClient: RequestHandler = (req: Request, res: Response) => {
     });
 };
 
-// Retrieve all Clients from the database.
-export const getAllClients: RequestHandler = (req: Request, res: Response) => {
-  // Calling the Sequelize findAll method. This is the same that a SELECT * FROM PRODUCT in a SQL query.
-  Client.findAll({include: [{
-    all: true,
-  }]})
-  .then((data: Client[]) => {
-    return res.status(200).json({
-      status: "success",
-      message: "Clients succesfully retrieved",
-      payload: data,
-    });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      status: "error",
-      message: "Something happened retrieving all clients. " + err.message,
-      payload: null,
-    });
-  });
-};
-
-// Find a single Client with an id
-export const getClientById: RequestHandler = (req: Request, res: Response) => {
-  Client.findByPk(req.params.id)
-  .then((data: Client | null) => {
-    return res.status(200).json({
-      status: "success",
-      message: "Clients successfully retrieved",
-      payload: data,
-    });
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      status: "error",
-      message: "Something happened retrieving all clients. " + err.message,
-      payload: null,
-    });
-  });
-};
-
 // Update a Client bny the id in the request
 export const modifyClient: RequestHandler = async (req: Request, res: Response) => {
   // Validate Request
@@ -83,41 +109,85 @@ export const modifyClient: RequestHandler = async (req: Request, res: Response) 
     });
   }
 
+  const { contract_pdf_url, logo_url, client_name, client_desc, exclusivity, high_growth, division } = req.body;
+
+  //Validations
+
+  if (!contract_pdf_url || !logo_url || !client_name || !client_desc || !exclusivity || !high_growth || !division) {
+    return res.status(400).json({
+        status: 'error',
+        message: 'All fields are required',
+        payload: null
+    });
+  }
+
+  if (!validator.isURL(contract_pdf_url)) {
+    return res.status(400).json({ message: 'Invalid URL format for contract' });
+  }
+
+  if (!validator.isURL(logo_url)) {
+    return res.status(400).json({ message: 'Invalid URL format for logo' });
+  }
+
   // Save Client in the database
-  Client.update({ ...req.body }, { where: { id: req.params.id }})
-  .then((isUpdated) => {
-    if (isUpdated) {
-      return res.status(200).json({
-        status: "success",
-        message: "Client succesfully updated",
-        payload: { ...req.body},
+  Client.findByPk(req.params.id)
+  .then((data: Client | null) => {
+    if (data) {
+      // Update the client
+      Client.update({...req.body}, {where: {id: req.params.id} })
+      .then((isUpdated) => {
+        if(isUpdated){
+          return res.status(200).json({
+            status: "success",
+            message: "Client updated successfully",
+            payload: {...req.body},
+          });
+        } else{
+          return res.status(500).json({
+            status: "error",
+            message: "There was an error updating the client",
+            payload: null,
+          });
+        }
       });
     } else {
-      return res.status(500).json({
-        status: "error",
-        message: "Something happened updating the client. ",
-        payload: null,      
+      return res.status(404).json({
+        status: 'error',
+        message: 'Client not found',
+        payload: null
       });
     }
-  })
-  .catch((err) => {
-    return res.status(500).json({
-      status: "error",
-      message: "Something happened updating a client. " + err.message,
-      payload: null,
-    });
   });
 };
 
 // Delete a Client with the specified if in the request 
 export const deleteClient: RequestHandler = async (req: Request, res: Response) => {
-  const { id } = req.body;
-  try {
-    await Client.destroy({ where: { id } });
-    return res.status(200).json({ message: "Client deleted" });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error deleting clients", error,
-    });
-  }
+  Client.findByPk(req.params.id)
+  .then((data: Client | null) => {
+    if (data) {
+      // Delete the client
+      Client.destroy({where: {id: req.params.id} })
+      .then((isDeleted) => {
+        if(isDeleted){
+          return res.status(200).json({
+            status: "success",
+            message: "Client deleted successfully",
+            payload: null,
+          });
+        } else{
+          return res.status(500).json({
+            status: "error",
+            message: "There was an error deleting the client",
+            payload: null,
+          });
+        }
+      });
+    } else {
+      return res.status(404).json({
+        status: 'error',
+        message: 'Client not found',
+        payload: null
+      });
+    }
+  });
 };
