53f8af8b4765f3f960c7fb824017ee3bf1a6a7a9
Andrés Martínez <autummata@gmail.com>
2024-04-14 13:02:59 -0700

    Add: CORS Method

--- index.ts
+++ index.ts
@@ -2,10 +2,13 @@ import express, { Express } from "express";
 import apiRouter from "./src/routes";
 import morgan from "morgan";
 import  connectionDB from "./src/connection/connection";
+import cors from "cors";
+
 
 const app: Express = express();
 const port = 3000;
-
+ 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(apiRouter);
